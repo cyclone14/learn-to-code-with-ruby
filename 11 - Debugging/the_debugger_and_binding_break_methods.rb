710584a8753348bdf9c284d683356295a5f55d8c
cyclone14 <jprice1947@comcast.net>
2025-05-09 22:54:12 -0400

    Chapter 11 complete

---
+++ 11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,16 @@
+require "debug"
+
+# Standard library - collection o9 code organized into libraries
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+# note - 'binding.break' is an alias for 'debugger'
+debugger
+
+beverage = "Arizona Iced Tea"
+puts "I love drinking #{beverage}"
+
+debugger
+
+puts "This is the end of the program"
+
