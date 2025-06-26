2c892ff32ad8ddaaf42157b35a649e7e66826830
cyclone14 <jprice1947@comcast.net>
2025-06-26 14:39:37 -0400

    Example of destructor logic. Called when an object is destroyed

--- 21 - Classes II/class_variables.rb
+++ 21 - Classes II/class_variables.rb
@@ -1,13 +1,29 @@
 # Class variable - data that lives on a class rather than an instance
+
+# The FINALIZER method doesn't get invoked until the object is
+# destroyed by the Garbage Collection (GC) process!!!
 class Bicycle
   @@count = 0
+  FINALIZER = lambda do |object_id|
+    @@count -= 1
+    p "finalizing %d" % object_id
+  end
 
   def self.count
     @@count
   end
+  def self.finalize(object_id)
+    p "finalizing %d!" % object_id
+  end
 
   def initialize
     @@count += 1
+    # ObjectSpace.define_finalizer(self, self.class.method(:finalize)) # works in 3.3
+    ObjectSpace.define_finalizer(self, FINALIZER)    # works in 3.3.5                  # works in 3.3
+    # ObjectSpace.define_finalizer(self, method(:finalize))              # doesn't work
+  end
+  def finalize(object_id)
+    p "finalizing %d!!" % object_id
   end
 
   def count
@@ -17,7 +33,11 @@ end
 
 p Bicycle.count
 b1 = Bicycle.new
-Bicycle.new
-Bicycle.new
+b2 = Bicycle.new
+b3 = Bicycle.new
 p Bicycle.count
 p b1.count
+b1 = nil # destroy (throw onto the garbage pile)
+b2 = nil # destroy (throw onto the garbage pile)
+GC.start
+p Bicycle.count
